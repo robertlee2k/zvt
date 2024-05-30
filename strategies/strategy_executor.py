c6559969722e3cc0774c6067bdd3c464f79a27df
robert <torobert@qq.com>
2024-05-31 00:00:15 +0800

    ROG

--- strategies/strategy_executor.py
+++ strategies/strategy_executor.py
@@ -9,7 +9,6 @@ class StrategyExecutor:
         self.aRe = aRe
         self.trading_cost = trading_cost
 
-
     def get_momentum_signals(self, rets, slow_window=12, fast_window=2):
         slow_mom = rets.rolling(slow_window).mean()
         fast_mom = rets.rolling(fast_window).mean()
@@ -27,7 +26,6 @@ class StrategyExecutor:
     def dynamic_trend_strategy(self, rets, slow_window=12, fast_window=2):
         slow_mom, fast_mom = self.get_momentum_signals(rets, slow_window, fast_window)
         market_state = self.get_market_state(slow_mom, fast_mom)
-
 
         # 计算本期头寸
         current_position = np.select([
@@ -48,7 +46,10 @@ class StrategyExecutor:
         previous_position = current_position_df.shift(1).fillna(0).values
 
         # 计算策略收益率,扣除交易成本
-        strategy_rets = rets * current_position - self.trading_cost * np.abs(current_position - previous_position)
+        strategy_rets = np.zeros_like(rets)
+        position_change = current_position - previous_position
+        strategy_rets[position_change == 0] = rets[position_change == 0] * previous_position[position_change == 0]
+        strategy_rets -= self.trading_cost * np.abs(position_change)
 
         # 记录每次策略执行的操作
         self.strategy_operations = defaultdict(list)
@@ -59,15 +60,16 @@ class StrategyExecutor:
             cum_bench_ret *= (1 + rets.loc[pd.Timestamp(date)])
 
             position_change = curr_pos - prev_pos
-            if position_change != 0 or date==rets.index[-1] or date==rets.index[0]:  # 只记录：有操作的日期、开始日期、结束日期
+            if position_change != 0 or date == rets.index[-1] or date == rets.index[0]:  # 只记录：有操作的日期、开始日期、结束日期
+                operation_date = date + pd.Timedelta(days=1)  # 执行操作的日期为发现信号的第二个交易日
                 if position_change > 0:
                     operation_type = "买入"
                 elif position_change < 0:
                     operation_type = "卖出"
                 else:
                     operation_type = "检查点"
-                self.strategy_operations[date.date()].append(
-                    (operation_type, position_change, curr_pos, cum_strategy_ret-1, cum_bench_ret-1))
+                self.strategy_operations[operation_date.date()].append(
+                    (operation_type, position_change, curr_pos, cum_strategy_ret - 1, cum_bench_ret - 1))
 
         # 返回策略收益率和头寸序列
         return strategy_rets, pd.Series(current_position, index=rets.index)
